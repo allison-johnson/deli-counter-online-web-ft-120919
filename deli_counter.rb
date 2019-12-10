91368e7c095c1891a5a361d06455e8aa218693c2
Allison Johnson <allison.cuttler@gmail.com>
2019-12-10 01:58:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(deli_line)
+  len = deli_line.size 
+  if len == 0 
+    puts "The line is currently empty."
+  else
+    str = "The line is currently: "
+    deli_line.each_with_index do |name, i|
+      str = str + "#{i}. #{name}"
+    end #each
+    str
+  end #if
+end #method line
